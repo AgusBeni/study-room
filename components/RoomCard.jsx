6f001081b6c890897d7fa3d4a05ae5d1ec08e997
Agus Zwitser <agus.zwitser@hva.nl>
2025-01-06 10:39:06 +0100

    fix: broken box fix

--- components/RoomCard.jsx
+++ components/RoomCard.jsx
@@ -9,28 +9,23 @@ const RoomCard = ({ post, handleEdit, handleDelete }) => {
   const router = useRouter();
 
   return (
-    <div className="flex-1 border border-black bg-white/20 bg-clip-padding p-6 w-full h-fit">
-     
-      <h1>{post.room}</h1>
+    <div className="break-inside-avoid">
+      <div className="flex-1 border border-black bg-white/20 bg-clip-padding p-6 w-full h-fit">
+        <h1>{post.room}</h1>
 
-      <p className="my-4 text-sm">{post.desc}</p>
+        <p className="my-4 text-sm">{post.desc}</p>
 
-      {session?.user.id === post.creator._id && pathName === "/profile" && (
-        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
-          <p
-            className="button1_color"
-            onClick={handleEdit}
-          >
-            Edit
-          </p>
-          <p
-            className="button1_color"
-            onClick={handleDelete}
-          >
-            Delete
-          </p>
-        </div>
-      )}
+        {session?.user.id === post.creator._id && pathName === "/profile" && (
+          <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+            <p className="button1_color" onClick={handleEdit}>
+              Edit
+            </p>
+            <p className="button1_color" onClick={handleDelete}>
+              Delete
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
