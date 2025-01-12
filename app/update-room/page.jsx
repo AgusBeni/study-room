12025c27f25eaefff4f766d3428120101c4193b6
Agus Zwitser <agus.benn@gmail.com>
2025-01-12 23:06:23 +0100

    fix: separated files

--- app/update-room/page.jsx
+++ app/update-room/page.jsx
@@ -1,73 +1,12 @@
-"use client";
+import { Suspense } from "react";
+import UpdateRoom from "./UpdateRoom";
 
-import { useEffect, useState, Suspense } from "react";
-
-import { useRouter, useSearchParams } from "next/navigation";
-
-import Form from "@components/Form";
-
-const UpdateRoom = () => {
-  const router = useRouter();
-
-  const searchParams = useSearchParams();
-  const roomId = searchParams.get("id");
-
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    room: "",
-    desc: "",
-  });
-
-  useEffect(() => {
-    const getRoomDetails = async () => {
-      const response = await fetch(`/api/room/${roomId}`);
-      const data = await response.json();
-
-      setPost({
-        room: data.room,
-        desc: data.desc,
-      });
-    };
-
-    if (roomId) getRoomDetails();
-  }, [roomId]);
-
-  const UpdateRoom = async (e) => {
-    e.preventDefault();
-    setSubmitting(true);
-
-    if (!roomId) return alert("Room ID not found!");
-
-    try {
-      const response = await fetch(`/api/room/${roomId}`, {
-        method: "PATCH",
-        body: JSON.stringify({
-          room: post.room,
-          desc: post.desc,
-        }),
-      });
-
-      if (response.ok) {
-        router.push("/");
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setSubmitting(false);
-    }
-  };
-
+const Page = ({ searchParams }) => {
   return (
-    <Suspense fallback={<div>Loaading....</div>}>
-      <Form
-        type="Edit"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={UpdateRoom}
-      />
+    <Suspense fallback={<div>Loaading.......</div>}>
+      <UpdateRoom roomId={searchParams.id} />
     </Suspense>
   );
 };
 
-export default UpdateRoom;
+export default Page;
---
+++ app/update-room/updateRoom.jsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { useRouter, useSearchParams } from "next/navigation";
+
+import Form from "@components/Form";
+
+const UpdateRoom = () => {
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const roomId = searchParams.get("id");
+
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    room: "",
+    desc: "",
+  });
+
+  useEffect(() => {
+    const getRoomDetails = async () => {
+      const response = await fetch(`/api/room/${roomId}`);
+      const data = await response.json();
+
+      setPost({
+        room: data.room,
+        desc: data.desc,
+      });
+    };
+
+    if (roomId) getRoomDetails();
+  }, [roomId]);
+
+  const UpdateRoom = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    if (!roomId) return alert("Room ID not found!");
+
+    try {
+      const response = await fetch(`/api/room/${roomId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          room: post.room,
+          desc: post.desc,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={UpdateRoom}
+    />
+  );
+};
+
+export default UpdateRoom;
